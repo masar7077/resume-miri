c66662510c83301bb3c15903ce81fba406092862
masar7077 <masar7077@gmail.com>
2025-04-14 15:07:23 +0330

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ const nav = document.querySelector('nav');
 hamburgerBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+// باز و بسته کردن توضیحات نمونه‌کار با کلیک روی کارت
+document.querySelectorAll('.portfolio-card').forEach(card => {
+  card.addEventListener('click', function () {
+    const moreText = this.querySelector('.more');
+    moreText.style.display = (moreText.style.display === 'block') ? 'none' : 'block';
+  });
+});
